806343243b30a31d99fe1ab474cce32380f775a3
吴呈兴 <1449697757@qq.com>
2021-07-24 16:40:03 +0800

    [新增]：继承

---
+++ 9-oop/extend/extend.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	name string
+	age  int
+}
+
+func (t *Human) Eat() {
+	fmt.Println("Human.Eat() ...")
+}
+
+func (t *Human) Walk() {
+	fmt.Println("Human.Walk() ...")
+}
+
+// 之类
+type SuperMan struct {
+	// 这样写就继承了父类
+	Human
+	level int
+}
+
+func (t *SuperMan) Eat() {
+	fmt.Println("SuperMan.Eat() ...")
+}
+
+func (t *SuperMan) Fly() {
+	fmt.Println("SuperMan.Fly() ...")
+}
+
+func main() {
+	human := Human{name: "wcx", age: 18}
+
+	human.Eat()
+	human.Walk()
+
+	fmt.Println("-------------------")
+	superMan := SuperMan{Human{name: "zyy", age: 18}, 1}
+
+	superMan.Walk() // 这里是父类的方法
+	superMan.Eat()  // 这里是 重写父类的方法
+	superMan.Fly()  // 这里是子类的方法
+}
